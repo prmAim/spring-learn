adaa4dc2d63a26ab44175d84e1f88905a5468b00
iankudinov <iankudinov@diasoft.ru>
2022-03-16 11:35:50 +0500

    Lesson 1 v1

---
+++ lesson-01/src/main/java/ru/geekbrains/TheFirstServlet.java
@@ -0,0 +1,51 @@
+package ru.geekbrains;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * Класс, реализует интерфейс Servlet
+ */
+@WebServlet(urlPatterns = "/myFirstServelet")    // шаблонб, который указывает какие URL будет обрабатывать сервелет
+public class TheFirstServlet implements Servlet {
+
+  // связь между сервером приложений и сервелетом
+  private ServletConfig сonfig;
+
+  /**
+   * Начало. Инициализация класса
+   */
+  @Override
+  public void init(ServletConfig servletConfig) throws ServletException {
+    this.сonfig = servletConfig;
+  }
+
+  @Override
+  public ServletConfig getServletConfig() {
+    return сonfig;
+  }
+
+  /**
+   * Метод обработки запросов
+   * @param servletRequest  - входящий запрос
+   * @param servletResponse - ответ на входящий запрос
+   */
+  @Override
+  public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+    servletResponse.getWriter().println("<h2>Hello world!!!</h2>>");
+  }
+
+  @Override
+  public String getServletInfo() {
+    return null;
+  }
+
+  /**
+   * Метод освобождения ресурсов
+   */
+  @Override
+  public void destroy() {
+
+  }
+}
